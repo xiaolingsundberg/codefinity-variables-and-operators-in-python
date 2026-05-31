97c652712ee18a9a9d0ebcfcb898ca35efce9a29
xiaolingsundberg <>
2026-05-31 21:13:42 +0000

    No content

---
+++ section/no_content/main.py
@@ -0,0 +1,13 @@
+# Number of classes and students per class
+classes = 6
+students = 45
+
+# Calculate total number of students
+total_students = classes*students
+
+# Calculate total number of classes
+total_classes = total_students/27
+
+# Testing
+print("Number of students:", total_students)
+print("Number of groups:", total_classes)
